c8398355584ae0c9e3981307c19a97a47320f7f0
MuhammadSRabbani <muhammadsrabbani@MacBook-Pro.local>
2020-06-03 23:08:55 +0700

    initialize framework libraries

--- App.js
+++ App.js
@@ -1,36 +1,23 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <Container>
+      <Text>Welcome to Pokedex!</Text>
+      <Text>To get started, edit App.js</Text>
+      <Button title="Explore" onPress={()=>console.log('Button....')} />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const Container = styled.View`
+  flex:1;
+  background-color: pink;  
+  align-items:center;
+  justify-content:center;
+`
+const Text = styled.Text`
+  font-size:24;
+`
+const Button = styled.Button`
+`
